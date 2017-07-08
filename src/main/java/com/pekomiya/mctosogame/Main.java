09398303f54627eac0be36edb052001b7e4d012e
pekomiya <peko@hogeclass.com>
2017-07-09 08:49:47 +0900

    init commit

---
+++ src/main/java/com/pekomiya/mctosogame/Main.java
@@ -0,0 +1,10 @@
+package com.pekomiya.mctosogame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
