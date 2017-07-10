0a64fa2a588392a45168b2dd9de9e02a84e68308
PekoMiyano <peko@hogeclass.com>
2017-07-10 16:28:48 +0900

    fix bug

--- src/main/java/com/pekomiya/mctosogame/Main.java
+++ src/main/java/com/pekomiya/mctosogame/Main.java
@@ -1,13 +1,11 @@
 package com.pekomiya.mctosogame;
 
-import com.pekomiya.mctosogame.command.TosoSettingCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
-        getCommand("tosogame").setExecutor(new TosoSettingCommand());
-
         super.onEnable();
     }
 }
+
