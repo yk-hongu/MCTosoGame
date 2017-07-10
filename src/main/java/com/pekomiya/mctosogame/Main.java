9258379e7aa42ca9c752913eaf2225a14412191c
pekomiya <peko@hogeclass.com>
2017-07-10 16:23:52 +0900

    plugin init

--- src/main/java/com/pekomiya/mctosogame/Main.java
+++ src/main/java/com/pekomiya/mctosogame/Main.java
@@ -1,10 +1,13 @@
 package com.pekomiya.mctosogame;
 
+import com.pekomiya.mctosogame.command.TosoSettingCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
+        getCommand("tosogame").setExecutor(new TosoSettingCommand());
+
         super.onEnable();
     }
 }
